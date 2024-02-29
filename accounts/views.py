91dff3d242a25a12b46436c6a331b18b37b139f9
Atom-mrbird <ataberkyakar@outlook.com>
2024-02-29 13:41:55 +0200

    @Copyright by Ataberk Yakar

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.db.migrations import serializer
 from django.urls import reverse_lazy
 from django.views import generic
+from rest_framework import response, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CardInformationSerializer
+import stripe
+
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
