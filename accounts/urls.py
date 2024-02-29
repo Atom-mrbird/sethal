bbe839f90af1870ca068c9496f9c03f2943952a6
Atom-mrbird <ataberkyakar@outlook.com>
2024-02-29 14:53:37 +0200

    @Copyright by Ataberk Yakar

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, ShopView
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("shop/", ShopView, name="shop"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.db.migrations import serializer
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
-from rest_framework import response, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .serializers import CardInformationSerializer
 import stripe
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def ShopView(request):
+    return render(request, 'shop.html')
