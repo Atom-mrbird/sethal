f7100c184da0e62e78ee727aa41a46b8c418e4d7
Atom-mrbird <ataberkyakar@outlook.com>
2024-03-07 20:47:35 +0200

    @Copyright by Ataberk Yakar

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import SignupForm, AddressForm
 
-
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
--- sethal/settings.py
+++ sethal/settings.py
@@ -133,7 +133,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "ataberkyakar@gmail.com"
-EMAIL_HOST_PASSWORD = "ATABERKy"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Sethal ataberkyakar@gmail.com"
